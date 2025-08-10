53ce7fa13202458ad57868abd37ed8ca7050fe16
DevHugoRS <eng.hugo.rs@gmail.com>
2025-08-10 15:57:39 +0200

    Start of app construction

---
+++ EmojiBliss/EmojiBliss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiBliss
+//
+//  Created by Hugo Rodrigues on 10/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiBliss/EmojiBliss/EmojiBlissApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiBlissApp.swift
+//  EmojiBliss
+//
+//  Created by Hugo Rodrigues on 10/08/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiBlissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
